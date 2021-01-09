077fb71ec076482f7f0f22ab8cf18533139ebd33
Samuel Eckermann <samuel.eckermann@maibornwolff.de>
2021-01-09 23:28:27 +0100

    added script for loading neo2 layout

--- setup_neo.sh
+++ setup_neo.sh
@@ -3,7 +3,6 @@
 download_dir="/usr/share/kbd/keymaps/i386/neo"
 mkdir -p ${download_dir}
 
-#curl -Lo ${download_dir}/neo.map https://git.neo-layout.org/neo/neo-layout/raw/branch/master/linux/console/neo.map
-wget -P ${download_dir} https://neo-layout.org/git/linux/console/neo.map
+curl -Lo ${download_dir}/neo.map https://git.neo-layout.org/neo/neo-layout/raw/branch/master/linux/console/neo.map
 
-localectl --no-convert set-keymap neo
+loadkeys neo
