b9de2013eaadcee6bd982a438e0b9e16dc325115
Samuel Eckermann <samuel.eckermann@maibornwolff.de>
2021-01-10 13:01:13 +0100

    bla

--- prepare_disks.sh
+++ prepare_disks.sh
@@ -147,7 +147,7 @@ doCreateLuks2() {
 	mkfs.ext4 -L "$LUKS_ROOT_NAME" "$LVM_DEVICE_PATH/$LUKS_ROOT_NAME"
 	mount "$LVM_DEVICE_PATH/$LUKS_ROOT_NAME" /mnt
 	mkdir /mnt/boot
-	moint  "$BOOT_DEVICE" /mnt/boot
+	mount  "$BOOT_DEVICE" /mnt/boot
 	mkdir /mnt/home
 }
 
